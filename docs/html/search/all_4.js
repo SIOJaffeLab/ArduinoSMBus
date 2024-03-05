020d9d547db1412d6b470cd5237113cc6c788c0c
cleeUC <68129277+cleeUC@users.noreply.github.com>
2024-03-05 11:22:30 -0500

    updated docs

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['manufacturedate_0',['manufactureDate',['../class_arduino_s_m_bus.html#a6ef1675a70f80131484dbbc3fa0dbb45',1,'ArduinoSMBus']]],
+  ['manufacturername_1',['manufacturerName',['../class_arduino_s_m_bus.html#a75bc281cbb7d438da06e7880b304876b',1,'ArduinoSMBus']]],
+  ['manufactureyear_2',['manufactureYear',['../class_arduino_s_m_bus.html#a716d79d59c632851b84719bafa57a47f',1,'ArduinoSMBus']]]
+];
